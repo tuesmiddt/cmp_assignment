6f2b99b8c3a623ef67aafc7e2a76b9d64832b3d8
Shaowei Chen <dev@chenshaowei.com>
2023-02-12 05:05:51 +0800

    Serve frontend from backend express server

--- api/src/server.ts
+++ api/src/server.ts
@@ -8,7 +8,6 @@ import path from "path";
 import helmet from "helmet";
 import express, { Request, Response, NextFunction } from "express";
 import logger from "jet-logger";
-import cors from "cors";
 
 import "express-async-errors";
 
@@ -31,15 +30,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(EnvVars.CookieProps.Secret));
-
-// CORS
-if (EnvVars.NodeEnv === NodeEnvs.Dev) {
-  app.use(
-    cors({
-      origin: "*",
-    })
-  );
-}
 
 // Show routes called in console during development
 if (EnvVars.NodeEnv === NodeEnvs.Dev) {
@@ -82,22 +72,7 @@ app.set("views", viewsDir);
 
 // Set static directory (js and css).
 const staticDir = path.join(__dirname, "public");
-app.use(express.static(staticDir));
-
-// Nav to login pg by default
-app.get("/", (_: Request, res: Response) => {
-  res.sendFile("login.html", { root: viewsDir });
-});
-
-// Redirect to login if not logged in.
-app.get("/users", (req: Request, res: Response) => {
-  const jwt = req.signedCookies[EnvVars.CookieProps.Key];
-  if (!jwt) {
-    res.redirect("/");
-  } else {
-    res.sendFile("users.html", { root: viewsDir });
-  }
-});
+app.use(express.static(staticDir, { extensions: ["html"] }));
 
 // **** Export default **** //
 
--- frontend/src/lib/api.tsx
+++ frontend/src/lib/api.tsx
@@ -1,4 +1,4 @@
-const ENDPOINT = 'http://localhost:8000/api';
+const ENDPOINT = '/api';
 
 export type Submission = {
   name: string;
